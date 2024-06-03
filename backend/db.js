d8a451457d4ce507df0eabbbe0290f4e1cf3e55b
PrabhleenKaur02 <prabhleen02info@gmail.com>
2024-06-03 13:50:02 +0530

    routes folder added

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,31 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://kaurprabhleen2002:EGiNeRa7yLhNuZc1@cluster0.ih4d0ql.mongodb.net/paytm");
 
-const userSchema = {
-    username: String,
-    firstName: String,
-    lastName: String,
-    password: String
-};
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  password: {
+    type: String,
+    required: true
+  }
+});
 
 const User = mongoose.model('User', userSchema);
 
