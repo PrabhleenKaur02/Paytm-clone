bc5669ee679e0b4d8a9b37a88a3020d6185ca12f
PrabhleenKaur02 <prabhleen02info@gmail.com>
2024-06-05 11:57:36 +0530

    auth middleware

---
+++ backend/middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require('./config');
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({msg: "wrong inputs"});
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        req.userId = decoded.userId;
+
+        next();
+
+    } catch (error) {
+        return res.status(403).json({
+            msg: "authorization failed. try again with correct inputs"
+        });
+    }
+};
+
+module.exports = authMiddleware;
